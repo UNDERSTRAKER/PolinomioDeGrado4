ab97c44d631182a8dbf1e1db86bcd7407ab5434f
Daniel Felipe Celis Molina <danielf-celism@unilibre.edu.co>
2025-02-27 12:15:26 -0500

    Correccion de intento de inferiores numeros a los superirores, intento de bucle con este condicional, variable tipo string que almacene valores de las condicionales que se cumplan

--- WpfApp1/Model/Ecuacion.cs
+++ WpfApp1/Model/Ecuacion.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfApp1.Model
 {
@@ -23,48 +24,51 @@ namespace WpfApp1.Model
         public List<double> Calcular()
         {
             List<double> resultados = new List<double>();
-
             double Xmin = CoefBoxMiMa[0];
             double Xmax = CoefBoxMiMa[1];
             double Ymin = CoefBoxMiMa[2];
             double Ymax = CoefBoxMiMa[3];
             double stepXY0 = 0.5;
-            double stepXY1 = -0.5;
-            if (Xmax > Xmin)
+            bool datosvalidos;
+            do
             {
-                for (double x = Xmin; x <= Xmax; x += stepXY0)
+                datosvalidos = true;
+                string mensajeError = "";
+                if (Xmin >= Xmax)
                 {
-                    for (double y = Ymin; y <= Ymax; y += stepXY0)
-                    {
-                        double ecu1 = Math.Cos(Coeficientes[0] * Math.Pow(CoefBoxMiMa[0], 3));
-                        double ecu2 = Math.Sin(Math.Pow(CoefBoxMiMa[0], 4) / Coeficientes[1]);
-                        double ecu3 = Coeficientes[2] * Math.Cos((Coeficientes[3] * CoefBoxMiMa[0]) + Math.Sin(Coeficientes[4] * CoefBoxMiMa[2]));
-                        double ecu4 = Math.Sqrt((Math.Pow(CoefBoxMiMa[0], 2) + Math.Pow(CoefBoxMiMa[2], 2)) / (5 * CoefBoxMiMa[0]));
-                        double ecu5 = Math.Sqrt((Coeficientes[5] * CoefBoxMiMa[0]) / CoefBoxMiMa[2] + Coeficientes[6]);
-                        double resultado = ecu1 - ecu2 * ecu3 + ecu4 - ecu5;
-                        resultados.Add(resultado);
-                    }                   
+                    mensajeError += ("El X Minimo debe ser menor y no igual o superior al X Maximo.");
+                    datosvalidos = false;
                 }
-                return resultados;
-            }
-            else
-            {
-                for (double x = Xmin; x >= Xmax; x += stepXY1)
+                if (Ymin >= Ymax)
                 {
-                    for (double y = Ymin; y >= Ymax; y += stepXY1)
-                    {
-                        double ecu1 = Math.Cos(Coeficientes[0] * Math.Pow(CoefBoxMiMa[0], 3));
-                        double ecu2 = Math.Sin(Math.Pow(y, 4) / Coeficientes[1]);
-                        double ecu3 = Coeficientes[2] * Math.Cos((Coeficientes[3] * CoefBoxMiMa[0]) + Math.Sin(Coeficientes[4] * y));
-                        double ecu4 = Math.Sqrt((Math.Pow(CoefBoxMiMa[0], 2) + Math.Pow(y, 2)) / (5 * CoefBoxMiMa[0]));
-                        double ecu5 = Math.Sqrt((Coeficientes[5] * CoefBoxMiMa[0]) / (y + Coeficientes[6]));
-                        double resultado = ecu1 - ecu2 * ecu3 + ecu4 - ecu5;
-                        resultados.Add(resultado);                      
-                    }                  
+                    mensajeError += (" El Y Minimo debe ser menor y no igual o superior al Y Maximo, porfavor reintente nuevamente.");
+                    datosvalidos = false;
                 }
-                return resultados;
-            }
+                if (!datosvalidos)
+                {
+                    MessageBox.Show($"{mensajeError}");
+                    continue;
+                }
+            } while (!datosvalidos);
+            
+           
 
+            for (double x = Xmin; x <= Xmax; x += stepXY0)
+            {
+
+                for (double y = Ymin; y <= Ymax; y += stepXY0)
+                {
+                     double ecu1 = Math.Cos(Coeficientes[0] * Math.Pow(CoefBoxMiMa[0], 3));
+                     double ecu2 = Math.Sin(Math.Pow(CoefBoxMiMa[0], 4) / Coeficientes[1]);
+                     double ecu3 = Coeficientes[2] * Math.Cos((Coeficientes[3] * CoefBoxMiMa[0]) + Math.Sin(Coeficientes[4] * CoefBoxMiMa[2]));
+                     double ecu4 = Math.Sqrt((Math.Pow(CoefBoxMiMa[0], 2) + Math.Pow(CoefBoxMiMa[2], 2)) / (5 * CoefBoxMiMa[0]));
+                     double ecu5 = Math.Sqrt((Coeficientes[5] * CoefBoxMiMa[0]) / CoefBoxMiMa[2] + Coeficientes[6]);
+                     double resultado = ecu1 - ecu2 * ecu3 + ecu4 - ecu5;
+                     resultados.Add(resultado);
+                }
+            }
+            return resultados;
+            
         }
     }
 }        
